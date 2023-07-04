0b951573c621af852ac7fecbc54c9e711e7e9f0c
syrineaboudi <106610890+syrineaboudi@users.noreply.github.com>
2023-07-04 02:21:25 +0100

    Update1

--- app/models/index.js
+++ app/models/index.js
@@ -4,13 +4,8 @@ mongoose.Promise = global.Promise;
 const db = {};
 
 db.mongoose = mongoose;
-
 db.user = require("./user.model");
-
 db.role = require("./role.model");
-
-
-
 db.ROLES = ["user", "admin", "moderator"];
 
 module.exports = db;
