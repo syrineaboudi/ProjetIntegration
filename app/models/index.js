03decc27d7754616af66ce001ebe10a47ca8c927
Khalil Guedoir <guedoiirkhalil@gmail.com>
2023-07-04 02:16:32 +0100

    Update

--- app/models/index.js
+++ app/models/index.js
@@ -1,13 +1,14 @@
-const dbConfig = require("../config/db.config.js");
-
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 const db = {};
+
 db.mongoose = mongoose;
-db.url = dbConfig.url;
 db.reclamations = require("./reclamation.model.js")(mongoose);
 db.notification = require("./notification.model.js")(mongoose);
 db.reponses = require("./reponse.model.js")(mongoose);
+
+
+
 
 module.exports = db;
