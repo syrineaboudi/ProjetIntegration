0039c39d2652be49e6de35f70718282ccc72fc40
syrineaboudi <106610890+syrineaboudi@users.noreply.github.com>
2023-07-04 02:13:25 +0100

    Update index.js

--- app/models/index.js
+++ app/models/index.js
@@ -9,11 +9,7 @@ db.user = require("./user.model");
 
 db.role = require("./role.model");
 
-db.products = require("./product.model.js")(mongoose);
 
-db.category = require("./category.model.js")(mongoose);
-
-db.dashboards = require("./dashboard.model.js")(mongoose);
 
 db.ROLES = ["user", "admin", "moderator"];
 
