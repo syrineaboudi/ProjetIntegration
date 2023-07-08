d66c28b59814f9e8c4e2527fe127440b5303b776
syrineaboudi <106610890+syrineaboudi@users.noreply.github.com>
2023-07-08 12:20:44 +0100

    Update auth.config.js

--- app/config/auth.config.js
+++ app/config/auth.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  secret: "Menumeric-caffe"
+  secret: "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",
+  googleClientId: "593654934494-nq6hdjuefv0lprgglrm9g4hsp3u4qv52.apps.googleusercontent.com",
 };
